c2bd62a0ae252f87630c0aeb3e0218e7056a94bc
Lee Raulin <leeraulin@gmail.com>
2019-05-14 09:37:33 -0400

    Increment minor version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='pyweather',
-    version='0.0.3',
+    version='0.0.4',
     entry_points={
         'console_scripts': ['pyweather=pyweather.main:main'],
     },
